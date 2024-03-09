95ab1d046af7dd9130b367bb2ef28808f1dee840
AbdelrahmanAttef74 <abdoeldox@gmail.com>
2024-03-09 17:03:28 +0200

    create counter state , counter cubit and create team increamet function

--- lib/cubits/counter_cubit.dart
+++ lib/cubits/counter_cubit.dart
@@ -1,5 +1,19 @@
+import 'package:basket_ball_counter/cubits/counter_states.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CounterCubit extends Cubit {
+class CounterCubit extends Cubit<CounterState> {
   CounterCubit(super.initialState);
+  int teamAPoints = 0;
+
+  int teamBPoints = 0;
+
+  void teamIncreament({required String team, required int teamNumber}) {
+    if (team == "A") {
+      teamAPoints += teamNumber;
+      emit(TeamAIncreamentState());
+    } else {
+      teamBPoints += teamNumber;
+      emit(TeamBIncreamentState());
+    }
+  }
 }
